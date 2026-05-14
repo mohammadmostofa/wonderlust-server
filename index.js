085e3fafe48e137cfc3598dc5426f81df8949bb9
mohammadmostofa <mohammadmostofa.dev@gmail.com>
2026-05-14 14:43:40 +0600

    added in mongodb and express connected

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
+
 const express = require('express')
+// dotenv file require
+const dotenv =  require('dotenv')
+// mongodb
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// call the dotenv config
+dotenv.config() 
+
 const app = express()
+const PORT = process.env.PORT || 5000 ;
 
-const PORT = 5000 
+const uri = process.env.MONGODB_URI;
 
+// mongodb
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (error) {
+    console.dir(error);
+  }
+  // ⚠️ finally এবং client.close() ফেলে দেওয়া হয়েছে যাতে কানেকশন বন্ধ না হয়
+}
+run().catch(console.dir);
 
 app.get('/' , (req,res) => {
   res.send("server is running fine!")
 })
 
+// run port
 app.listen(PORT,() => {
-  console.log(`surver running on Port ${PORT}`)
-} )
+  console.log(`server running on Port ${PORT}`)
+})
